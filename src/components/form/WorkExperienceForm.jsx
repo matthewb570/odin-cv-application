24d396ca44af2fe94e432eb2f1861a225c09aaa2
Matthew Burgess <boog397@gmail.com>
2025-03-23 00:25:03 -0500

    Add validation for WorkExperienceForm

--- src/components/form/WorkExperienceForm.jsx
+++ src/components/form/WorkExperienceForm.jsx
@@ -3,11 +3,99 @@ import FormInput from "../input/FormInput";
 import FormSelect from "../input/FormSelect";
 import { useState } from "react";
 import Job from "../../objects/Job";
+import StringUtils from "../../utils/StringUtils";
 
 export default function WorkExperienceForm({ jobs, setJobs, onSubmit }) {
   const [selectedJobId, setSelectedJobId] = useState(jobs[0].id);
 
+  const [isCompanyNameValid, setIsCompanyNameValid] = useState(undefined);
+  const [isPositionTitleValid, setIsPositionTitleValid] = useState(undefined);
+  const [isJobResponsibilitiesValid, setIsJobResponsibilitiesValid] =
+    useState(undefined);
+  const [isStartDateValid, setIsStartDateValid] = useState(undefined);
+  const [isEndDateValid, setIsEndDateValid] = useState(undefined);
+
   const selectedJobIndex = jobs.findIndex((job) => job.id === selectedJobId);
+
+  function validateCompanyName(companyName) {
+    const newIsCompanyNameValid = !StringUtils.isEmpty(companyName);
+    setIsCompanyNameValid(newIsCompanyNameValid);
+    return newIsCompanyNameValid;
+  }
+
+  function validatePositionTitle(positionTitle) {
+    const newIsPositionTitleValid = !StringUtils.isEmpty(positionTitle);
+    setIsPositionTitleValid(newIsPositionTitleValid);
+    return newIsPositionTitleValid;
+  }
+
+  function validateJobResponsibilities(jobResponsibilities) {
+    const newIsJobResponsibilitiesValid =
+      !StringUtils.isEmpty(jobResponsibilities);
+    setIsJobResponsibilitiesValid(newIsJobResponsibilitiesValid);
+    return newIsJobResponsibilitiesValid;
+  }
+
+  function validateStartDate(startDate, endDate, doValidateEndDate) {
+    if (doValidateEndDate && !StringUtils.isEmpty(endDate)) {
+      validateEndDate(endDate);
+    }
+
+    const newIsStartDateValid =
+      !StringUtils.isEmpty(startDate) &&
+      (!StringUtils.isEmpty(endDate) ? startDate <= endDate : true);
+    setIsStartDateValid(newIsStartDateValid);
+    return newIsStartDateValid;
+  }
+
+  function validateEndDate(endDate, startDate, doValidateStartDate) {
+    if (doValidateStartDate && !StringUtils.isEmpty(startDate)) {
+      validateStartDate(startDate, endDate, false);
+    }
+
+    const newIsEndDateValid =
+      !StringUtils.isEmpty(endDate) &&
+      (!StringUtils.isEmpty(startDate) ? endDate >= startDate : true);
+    setIsEndDateValid(newIsEndDateValid);
+    return newIsEndDateValid;
+  }
+
+  function validateForm() {
+    const isCompanyNameValid = validateCompanyName(
+      jobs[selectedJobIndex].companyName,
+    );
+    const isPositionTitleValid = validatePositionTitle(
+      jobs[selectedJobIndex].positionTitle,
+    );
+    const isJobResponsibilitiesValid = validateJobResponsibilities(
+      jobs[selectedJobIndex].jobResponsibilities,
+    );
+    const isStartDateValid = validateStartDate(
+      jobs[selectedJobIndex].startDate,
+      jobs[selectedJobIndex].endDate,
+      false,
+    );
+    const isEndDateValid = validateEndDate(
+      jobs[selectedJobIndex].endDate,
+      jobs[selectedJobIndex].startDate,
+      false,
+    );
+    return (
+      isCompanyNameValid &&
+      isPositionTitleValid &&
+      isJobResponsibilitiesValid &&
+      isStartDateValid &&
+      isEndDateValid
+    );
+  }
+
+  function resetValidity() {
+    setIsCompanyNameValid(undefined);
+    setIsPositionTitleValid(undefined);
+    setIsJobResponsibilitiesValid(undefined);
+    setIsStartDateValid(undefined);
+    setIsEndDateValid(undefined);
+  }
 
   function setCompanyName(companyName) {
     const updatedJobs = [...jobs];
@@ -40,12 +128,16 @@ export default function WorkExperienceForm({ jobs, setJobs, onSubmit }) {
   }
 
   function handleAddClick() {
-    const newJob = new Job();
-    setJobs([...jobs, newJob]);
-    setSelectedJobId(newJob.id);
+    if (validateForm()) {
+      resetValidity();
+      const newJob = new Job();
+      setJobs([...jobs, newJob]);
+      setSelectedJobId(newJob.id);
+    }
   }
 
   function handleDeleteClick() {
+    resetValidity();
     const updatedJobs = [...jobs];
     updatedJobs.splice(selectedJobIndex, 1);
     setJobs(updatedJobs);
@@ -56,14 +148,21 @@ export default function WorkExperienceForm({ jobs, setJobs, onSubmit }) {
     );
   }
 
+  function handleSelectChange(value) {
+    if (validateForm()) {
+      resetValidity();
+      setSelectedJobId(value);
+    }
+  }
+
   return (
-    <Form onSubmit={onSubmit}>
+    <Form validationFunction={validateForm} onSubmit={onSubmit}>
       <div className="form-row">
         <FormSelect
           label={"Company"}
           name={"selectedJob"}
           value={selectedJobId}
-          setValue={setSelectedJobId}
+          onChange={handleSelectChange}
           required={false}
           options={jobs.map((job) => {
             return {
@@ -82,48 +181,55 @@ export default function WorkExperienceForm({ jobs, setJobs, onSubmit }) {
       </div>
       <FormInput
         type={"text"}
-        label={"Company Name"}
+        label={"Company Name *"}
         name={"companyName"}
         value={jobs[selectedJobIndex].companyName}
         setValue={setCompanyName}
         placeholder={"Generic Software Development Company"}
-        required={true}
+        isValid={isCompanyNameValid}
+        validationFunction={validateCompanyName}
       />
       <FormInput
         type={"text"}
-        label={"Position Title"}
+        label={"Position Title *"}
         name={"positionTitle"}
         value={jobs[selectedJobIndex].positionTitle}
         setValue={setPositionTitle}
         placeholder={"Software Engineer"}
-        required={true}
+        isValid={isPositionTitleValid}
+        validationFunction={validatePositionTitle}
       />
       <FormInput
         type={"textarea"}
-        label={"Job Responsibilities"}
+        label={"Job Responsibilities *"}
         name={"jobResponsibilities"}
         value={jobs[selectedJobIndex].jobResponsibilities}
         setValue={setJobResponsibilities}
         placeholder={"Time on the job includes..."}
-        required={true}
+        isValid={isJobResponsibilitiesValid}
+        validationFunction={validateJobResponsibilities}
       />
       <FormInput
         type={"month"}
-        label={"Employment Start Date"}
+        label={"Employment Start Date *"}
         name={"employmentStartDate"}
         value={jobs[selectedJobIndex].startDate}
         setValue={setStartDate}
-        placeholder={"mm/dd/yyyy"}
-        required={true}
+        isValid={isStartDateValid}
+        validationFunction={(startDate) =>
+          validateStartDate(startDate, jobs[selectedJobIndex].endDate, true)
+        }
       />
       <FormInput
         type={"month"}
-        label={"Employment End Date"}
+        label={"Employment End Date *"}
         name={"employmentEndDate"}
         value={jobs[selectedJobIndex].endDate}
         setValue={setEndDate}
-        placeholder={"mm/dd/yyyy"}
-        required={true}
+        isValid={isEndDateValid}
+        validationFunction={(endDate) =>
+          validateEndDate(endDate, jobs[selectedJobIndex].startDate, true)
+        }
       />
     </Form>
   );
