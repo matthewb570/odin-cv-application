82db52465136a286b3be5f5d0d9e7d02d2d92462
Matthew Burgess <boog397@gmail.com>
2025-03-25 21:57:50 -0500

    Optimize for mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header({ onEditAll }) {
   return (
     <header>
-      <div></div>
+      <div className="mobile-hidden"></div>
       <h1>Resume Builder</h1>
       <div className="button-list">
         <button type="button" className="edit" onClick={onEditAll}>
